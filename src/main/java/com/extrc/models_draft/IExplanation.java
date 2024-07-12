5376afb89f3b0cdcd66b4812eae57a1dbd4c0509
Thabo Vincent Moloi <thabomoloicodes@gmail.com>
2024-07-12 04:05:26 +0200

    refactor: remove drafts

--- src/main/java/com/extrc/models_draft/IExplanation.java
+++
@@ -1,9 +0,0 @@
-package com.extrc.models_draft;
-
-public interface IExplanation {
-  public void addSubscriber(IObserver observer);
-
-  public void removeSubscriber(IObserver observer);
-
-  public void notifySubscribers(String explanation);
-}
--- src/main/java/com/extrc/models_draft/IObserver.java
+++
@@ -1,5 +0,0 @@
-package com.extrc.models_draft;
-
-public interface IObserver {
-  public void notification(String who, String what);
-}
