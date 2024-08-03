f6ca14c84ea85c0d5ef5a649a004f290caacbd2e
Thabo Vincent Moloi <thabomoloicodes@gmail.com>
2024-08-03 05:33:56 +0200

    chore: add blank tabs

--- src/main/web/src/components/MainContent.tsx
+++ src/main/web/src/components/MainContent.tsx
@@ -1,17 +1,10 @@
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import QueryCard from "@/components/QueryCard";
-import KnowledgeBaseCard from "./KnowledgeBaseCard";
+import KnowledgeBaseCard from "@/components/KnowledgeBaseCard";
+import Summary from "@/components/main-tabs/Summary";
+import BaseRank from "@/components/main-tabs/BaseRank";
+import RationalClosure from "@/components/main-tabs/RationalClosure";
+import LexicographicClosure from "@/components/main-tabs/LexicographicClosure";
 
 export default function MainContent() {
   return (
@@ -20,56 +13,26 @@ export default function MainContent() {
         <QueryCard />
         <KnowledgeBaseCard />
       </div>
-      <Tabs defaultValue="account">
-        <TabsList className="grid w-full grid-cols-2">
-          <TabsTrigger value="account">Account</TabsTrigger>
-          <TabsTrigger value="password">Password</TabsTrigger>
+      <Tabs defaultValue="summary">
+        <TabsList className="grid w-full grid-cols-4">
+          <TabsTrigger value="summary">Summary</TabsTrigger>
+          <TabsTrigger value="baseRank">Base Rank</TabsTrigger>
+          <TabsTrigger value="rationaClosure">Rational Closure</TabsTrigger>
+          <TabsTrigger value="lexicographicClosure">
+            Lexicographic Closure
+          </TabsTrigger>
         </TabsList>
-        <TabsContent value="account">
-          <Card>
-            <CardHeader>
-              <CardTitle>Account</CardTitle>
-              <CardDescription>
-                Make changes to your account here. Click save when you're done.
-              </CardDescription>
-            </CardHeader>
-            <CardContent className="space-y-2">
-              <div className="space-y-1">
-                <Label htmlFor="name">Name</Label>
-                <Input id="name" defaultValue="Pedro Duarte" />
-              </div>
-              <div className="space-y-1">
-                <Label htmlFor="username">Username</Label>
-                <Input id="username" defaultValue="@peduarte" />
-              </div>
-            </CardContent>
-            <CardFooter>
-              <Button>Save changes</Button>
-            </CardFooter>
-          </Card>
+        <TabsContent value="summary">
+          <Summary />
         </TabsContent>
-        <TabsContent value="password">
-          <Card>
-            <CardHeader>
-              <CardTitle>Password</CardTitle>
-              <CardDescription>
-                Change your password here. After saving, you'll be logged out.
-              </CardDescription>
-            </CardHeader>
-            <CardContent className="space-y-2">
-              <div className="space-y-1">
-                <Label htmlFor="current">Current password</Label>
-                <Input id="current" type="password" />
-              </div>
-              <div className="space-y-1">
-                <Label htmlFor="new">New password</Label>
-                <Input id="new" type="password" />
-              </div>
-            </CardContent>
-            <CardFooter>
-              <Button>Save password</Button>
-            </CardFooter>
-          </Card>
+        <TabsContent value="baseRank">
+          <BaseRank />
+        </TabsContent>
+        <TabsContent value="rationaClosure">
+          <RationalClosure />
+        </TabsContent>
+        <TabsContent value="lexicographicClosure">
+          <LexicographicClosure />
         </TabsContent>
       </Tabs>
     </div>
---
+++ src/main/web/src/components/main-tabs/BaseRank.tsx
@@ -0,0 +1,3 @@
+export default function BaseRank() {
+  return <div>BaseRank</div>;
+}
